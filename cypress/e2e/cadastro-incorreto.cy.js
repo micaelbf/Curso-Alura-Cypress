c7ddef9544b7ffabf038ff73f03f41b067a66c4e
micaelbf <micaelbferreira@gmail.com>
2024-06-21 13:23:50 -0300

    add cadastro incorreto

---
+++ cypress/e2e/cadastro-incorreto.cy.js
@@ -0,0 +1,12 @@
+describe('Verificação página de cadastro', () => {
+
+    it('Deve preencher os campos corretamente para cadastro de um novo usuário', () => {
+      cy.visit('https://adopet-frontend-cypress.vercel.app/');
+      cy.get('[data-test="register-button"]').click();
+      cy.get('[data-test="submit-button"]').click();
+      cy.contains('É necessário informar um endereço de email').should('be.visible');
+      cy.contains('Crie uma senha').should('be.visible');  
+      cy.contains('Repita a senha criada acima').should('be.visible');  
+    })
+  })
+  
