3faca409aac93a64fe64777b5e0bc4c6a38e24e0
micaelbf <micaelbferreira@gmail.com>
2024-06-21 13:01:59 -0300

    add mais artquivos no gitignore

--- cypress/e2e/cadastro-correto.cy.js
+++ cypress/e2e/cadastro-correto.cy.js
@@ -2,11 +2,11 @@ describe('Verificação página de cadastro', () => {
 
   it('Deve preencher os campos corretamente para cadastro de um novo usuário', () => {
     cy.visit('https://adopet-frontend-cypress.vercel.app/');
-    cy.contains('a','Cadastrar').click();
-    cy.get('input[name=nome]').type('Ana de Jesus');
-    cy.get('input[name=email]').type('ana@email.com');
-    cy.get('input[name=password]').type('Senha123');
-    cy.get('input[name=confirm_password]').type('Senha123');
-    cy.contains('button','Cadastrar').click();
+    cy.get('[data-test="register-button"]').click();
+    cy.get('[data-test="input-name"]').type('Ana de Jesus');
+    cy.get('[data-test="input-email"]').type('ana@email.com');
+    cy.get('[data-test="input-password"]').type('Senha123');
+    cy.get('[data-test="input-confirm-password"]').type('Senha123');
+    cy.get('[data-test="submit-button"]').click();
   })
 })
