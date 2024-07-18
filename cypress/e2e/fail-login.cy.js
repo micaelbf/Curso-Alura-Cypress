6aaa879250209561dbce8883c3cbff1b17a4fdeb
micaelbf <micaelbferreira@gmail.com>
2024-07-18 10:38:25 -0300

    Alterar estrutura do comentarios

--- cypress/e2e/fail-login.cy.js
+++ cypress/e2e/fail-login.cy.js
@@ -1,17 +1,10 @@
 // Scenario: Login fail site Adopet
-
 //GIVE: A user acesses logins page.
-
 //WHEN: A user insert a e-mail and password non-standard.
-
 //AND: A user click on login. 
-
 //THEN: The system valid credencials 
-
 //AND: System display error menssage "Por favor, verifique o e-mail digitado" e/ou "A senha deve conter pelo menos uma letra maiúscula, um número e ter entre 6 e 15 caracteres".
-
 //Business Rules 
-
 // The e-mail's need to be a valid format. 
 // The password must be capital letter, a number and have between 6 and 15 characters.
 
